9a4a62bcab93a40f069a0cf189baa0789c687e20
rgpass <rgpass@gmail.com>
2015-03-01 13:14:05 -0500

    Fix API route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    scope module: :v1 do
+    namespace :v1 do
       get '/:year/makes', to: 'cars#makes'
     end
   end
