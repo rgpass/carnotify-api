e1fb9ae3fb15708fce03d12954a13bc73a0d8a0b
rgpass <rgpass@gmail.com>
2015-03-04 16:39:56 -0500

    Fix route

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,17 +1,11 @@
 class Api::V1::CarsController < ApplicationController
-  # respond_to :json
-
   def makes
-    year = params[:year]
-    edmunds_makes = HTTParty.get("http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=jfc7fztasc6vfbaqzmfnw484&year=#{year}")
-    render json: edmunds_makes, status: 200#, location: [:api, user]
+    edmunds_makes = Car.makes_by_year(params[:year])
+    render json: edmunds_makes, status: 200
   end
 
   def maintenance
     model_year_id = params[:modelyearid]
-    maintenance_list = HTTParty.get("https://api.edmunds.com/v1/api/maintenance/actionrepository/findbymodelyearid?modelyearid=#{model_year_id}&fmt=json&api_key=jfc7fztasc6vfbaqzmfnw484")['actionHolder']
-    maintenance_list.sort_by! { |item| item['intervalMileage'] }
-    intervals = maintenance_list.map { |item| item['intervalMileage'] }.uniq
-    @maintenance_list = intervals.map { |interval| [{ mileage: interval, items: maintenance_list.select { |item| item['intervalMileage'] == interval }}] }
+    @maintenance_list = Car.maintenance_list_for_model_year_id(model_year_id)
   end
 end
---
+++ app/models/car.rb
@@ -0,0 +1,12 @@
+class Car
+  def self.makes_by_year(year)
+    HTTParty.get("http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=jfc7fztasc6vfbaqzmfnw484&year=#{year}")
+  end
+
+  def self.maintenance_list_for_model_year_id(model_year_id)
+    maintenance_list = HTTParty.get("https://api.edmunds.com/v1/api/maintenance/actionrepository/findbymodelyearid?modelyearid=#{model_year_id}&fmt=json&api_key=jfc7fztasc6vfbaqzmfnw484")['actionHolder']
+    maintenance_list.sort_by! { |item| item['intervalMileage'] }
+    intervals = maintenance_list.map { |item| item['intervalMileage'] }.uniq
+    intervals.map { |interval| [{ mileage: interval, items: maintenance_list.select { |item| item['intervalMileage'] == interval }}] }
+  end
+end
