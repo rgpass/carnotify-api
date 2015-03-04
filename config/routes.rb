53984e91f2dc555e56b28faf76592dbe78549199
rgpass <rgpass@gmail.com>
2015-03-04 18:48:53 -0500

    Clean up cars#maintenance

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get '/makes', to: 'cars#makes'
-      get '/maintenance/:modelyearid', to: 'cars#maintenance'
+      get '/maintenance', to: 'cars#maintenance'
     end
   end
 end
