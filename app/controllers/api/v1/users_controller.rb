95988f7e87a80581d99ff549f4e9a3ce69cf3a36
rgpass <rgpass@gmail.com>
2015-03-12 08:30:42 -0400

    Add users#index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
 
+  def index
+    render json: User.all, status: 200
+  end
+
   def show
     respond_with User.find(params[:id])
   end
